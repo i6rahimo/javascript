173c483d545a74a8de0b6f201e81961325c43f72
i6rahim <i6rahim210@gmail.com>
2021-12-11 17:24:08 +0300

    training

--- js/main.js
+++ js/main.js
@@ -162,20 +162,171 @@
 // console.log(calculatr.mmul())
 
 
-function User(name) {
-    this.name = name,
-    this.isAdmin = false
+// function User(name) {
+//     this.name = name,
+//     this.isAdmin = false
+// }
+
+// let user = new User('Вася')
+// let admin = new User('Катя')
+// console.log(user.name)
+// console.log(admin.name)
+
+// function BigUser() {
+//     this.name = "Alex";
+
+//     return { name: "Godzilla"}
+// }
+
+// console.log( new BigUser().name)
+
+
+
+// function pow( x , n) {
+//     let result = 1;
+
+//     for (let i = 10; i < n; i++ ) {
+//         result *= x
+//     }
+    
+//     return result
+// }
+
+// console.log( pow( 2, 13 ))
+
+
+
+// function pow2( x, n) {
+//     if(n==1) {
+//         return x
+//     }else {
+//         return x * pow2(x, n -1)
+//     }
+// }
+
+// console.log(pow2(2, 3))
+
+
+
+// function getFactorial(n) {
+//     if (n === 1) {
+//         return 1
+//     }else {
+//         return n * getFactorial(n - 1)
+//     }
+
+// }
+
+// var result = getFactorial(4)
+// console.log(result)
+
+
+// function doA(n) {
+//     return doA(n - 1)
+// }
+// var result = doA(4)
+// console.log(result)\
+
+
+// function sumTo(n) {
+//     if (n === 0 ) {
+//         return n
+//     }else {
+//         return n * (n - 1)
+//     }
+// }
+
+// console.log(sumTo(3))
+
+
+// function sumAll(...args) {
+//     let sum = 0;
+
+//     for (let arg of args) sum += arg;
+//     return sum;
+// }
+
+// console.log(sumAll(12, 12,123123123, 12));
+
+
+
+
+// function king(firstName, lastName, ...titles) {
+//     console.log( firstName + ' ' + lastName)
+//     console.log(titles[0]);
+// }
+
+// king("Юлий", "Цезарь", "Kind", " Kind", " Kind")
+
+
+
+// function someFc() {
+//     console.log(arguments[0]);
+//     console.log(arguments[1]);
+//     console.log(arguments.length);
+// }
+
+// console.log('Jlw', 'qwe', 123, 12312, "qwee");
+
+
+
+// function sayHi() {
+//     console.log('hi');
+// }
+
+// console.log(sayHi.name);
+
+// function ask(question, ...handlers) {
+//     let isYes = confirm(question)
+//     for(let handler of handlers) {
+//         if(handler.length == 0) {
+//             if(isYes) handler()
+//         }else {
+//             handler(isYes)
+//         }
+//     }
+// }
+
+// alert("Вопрос?", () => alert("Вы ответили да"), result => alert(result))
+
+
+
+
+
+
+
+
+const namelist = new Array(4294967269)
+
+console.log(namelist.length);
+
+let namelistB = []
+namelistB.length = Math.pow(2, 32) -1
+console.log(namelistB);
+
+let arr = [1, 2, 3]
+printEntries(arr)
+
+arr.length = 5
+printEntries(arr)
+
+function printEntries(arr) {
+    let goNext = true
+    let entries = arr.entries()
+    while (goNext) {
+        let result = entries.next() 
+        if(result.done !== true) {
+            console.log(result.value[1]);
+            goNext = true
+        }else 
+        goNext = false
+    }
+    console.log('=== printed ===');
 }
 
-let user = new User('Вася')
-let admin = new User('Катя')
-console.log(user.name)
-console.log(admin.name)
+let obj = { foo: 'bar', baz: 42}
+console.log(Object.entries(obj));
 
-function BigUser() {
-    this.name = "Alex";
-
-    return { name: "Godzilla"}
-}
-
-console.log( new BigUser().name)
+let myObj = Object.create({}, { getFoo: {value: function() {return this.foo}}})
+myObj.foo = 'bar'
+console.log(Object.entries(myObj));
