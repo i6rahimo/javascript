489b9d72206730e48378c49d08367ca9d88e0fbf
i6rahim <i6rahim210@gmail.com>
2022-02-14 19:27:06 +0300

    "soving"

--- questions/main.js
+++ questions/main.js
@@ -20,23 +20,35 @@
 // }
 
 
-function makeArmy() {
+// function makeArmy() {
 
-  let shooters = [];
+//   let shooters = [1];
 
-  let i = 0;
-  while (i < 10) {
-    let shooter = function () { // функция shooter
-      console.log(i);; // должна выводить порядковый номер
-    };
-    shooters.push(shooter);
-    i++;
-  }
-  return shooters;
+//   let i = 0;
+//   while (i < 10) {
+//     let shooter = function () { // функция shooter
+//       console.log(i);; // должна выводить порядковый номер
+//     };
+//     shooters.push(shooter);
+//     i++;
+//   }
+//   return shooters;
+// }
+// makeArmy()
+// let army = makeArmy();
+
+// army[0](); // у 0-го стрелка будет номер 10
+// army[5](); // и у 5-го стрелка тоже будет номер 10
+// // ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...
+
+
+const shape = {
+  radius: 10,
+  diametr() {
+    return this.radius * 2;
+  },
+  perimeter: () => 2 * Math.PI * this.radius
 }
 
-let army = makeArmy();
-
-army[0](); // у 0-го стрелка будет номер 10
-army[5](); // и у 5-го стрелка тоже будет номер 10
-// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...
+shape.diametr()
+shapr.perimeter()
