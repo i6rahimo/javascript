8a25b5af0dbbe6353066c9c3f73cfe0986fd83a9
i6rahim <i6rahim210@gmail.com>
2022-02-24 18:46:36 +0300

    "create wordcunter"

--- password_visibillity/main.js
+++ password_visibillity/main.js
@@ -2,9 +2,12 @@ const togglePassword = document.querySelector("#togglePassword")
 const password = document.querySelector('#password')
 
 
+console.log(this);
+
 togglePassword.addEventListener('click', function () {
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
+    console.log(this);
     this.classList.toggle("bi-eye")
 })
 
